cf3716b27a01b90d9d1eb9fd463b0faf593d1bd6
John Terrell <john.terrell@gmail.com>
2019-02-27 21:40:06 -0800

    Fixes to Riley0 decoder based on simulation.

--- Riley0/hardware/address_decoder.v
+++ Riley0/hardware/address_decoder.v
@@ -7,7 +7,7 @@ module address_decoder(
 	output rom_sel
 	);
 
-wire [7:0] upper_nibble_enable = 7'1;
+wire [7:0] upper_nibble_enable;
 	
 ic_74138 u7(
 	.A(address[14:12]),
@@ -18,14 +18,21 @@ ic_74138 u7(
 	);
 
 assign io_sel = upper_nibble_enable[5];
-
+	
+wire rom_line_and;	
+	
 ic_7400 u8(
+	.A1(St0),
+	.B1(St0),
 	.A2(io_sel),
 	.B2(rom_sel),
 	.Y2(ram_sel),
-	.A3(upper_nibble_enable[6]),
+	.A3(upper_nibble_enable[6]),  // A3/B3 and A4/B4 are used to implement an AND gate.
 	.B3(upper_nibble_enable[7]),
-	.Y3(rom_sel)
+	.Y3(rom_line_and),
+	.A4(rom_line_and),
+	.B4(rom_line_and),
+	.Y4(rom_sel)
 	);
 
 endmodule
@@ -36,7 +43,7 @@ module address_decoder_tb;
 	wire ram_sel;
 	wire rom_sel;
 	wire io_sel;
-
+	
 	reg clk;
 	
 	// duration for each bit = 20 * timescale = 20 * 1 ns  = 20ns
@@ -49,7 +56,6 @@ module address_decoder_tb;
 		.io_sel(io_sel)
 		);
 
-		
 	always 
 	begin
 		 clk = 1'b1; 
@@ -62,28 +68,28 @@ module address_decoder_tb;
 	always @(posedge clk)
 	begin
 		address = 16'b0000000000000000;
-		#period; // wait for period
+		#5; // wait for period
 		// display message if output not matched
 		if(ram_sel != 0 && rom_sel != 1 && io_sel != 1)  
-			$display("Test failed for $0000");
+			$stop;
 
-		address = 16'b0010000000000000;
+		address = 16'b1101000000000000;
 		#period; // wait for period
 		// display message if output not matched
 		if(ram_sel != 1 && rom_sel != 1 && io_sel != 0)  
-			$display("Test failed for $D000");
+			$stop;
 
-		address = 16'b0100000000000000;
+		address = 16'b1110000000000000;
 		#period; // wait for period
 		// display message if output not matched
 		if(ram_sel != 1 && rom_sel != 0 && io_sel != 1)  
-			$display("Test failed for $E000");
+			$stop;
 
-		address = 16'b1000000000000000;
+		address = 16'b1111000000000000;
 		#period; // wait for period
 		// display message if output not matched
 		if(ram_sel != 1 && rom_sel != 0 && io_sel != 1)  
-			$display("Test failed for $F000");
+			$stop;
 			
 		$stop;   // end of simulation
 	end
